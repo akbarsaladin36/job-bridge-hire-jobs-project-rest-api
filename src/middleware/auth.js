5d2f70ede14c05f966a726e8e9d6f0db09b9d3ee
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-05-16 06:50:04 +0700

    fix authentication for worker or company

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -27,5 +27,55 @@ module.exports = {
     } else {
       return helper.response(res, 403, 'Please login first !')
     }
+  },
+
+  isWorker: (req, res, next) => {
+    let token = req.headers.authorization
+
+    if (token) {
+      token = token.split(' ')[1]
+      jwt.verify(token, process.env.PRIVATE_KEY, (error, result) => {
+        if (
+          (error && error.name === 'JsonWebTokenError') ||
+          (error && error.name === 'TokenExpiredError')
+        ) {
+          return helper.response(res, 403, error.message)
+        } else {
+          req.decodeToken = result
+          if (req.decodeToken.id_worker) {
+            next()
+          } else {
+            return helper.response(res, 403, 'Youre not a worker !')
+          }
+        }
+      })
+    } else {
+      return helper.response(res, 403, 'Please login first !')
+    }
+  },
+
+  isCompany: (req, res, next) => {
+    let token = req.headers.authorization
+
+    if (token) {
+      token = token.split(' ')[1]
+      jwt.verify(token, process.env.PRIVATE_KEY, (error, result) => {
+        if (
+          (error && error.name === 'JsonWebTokenError') ||
+          (error && error.name === 'TokenExpiredError')
+        ) {
+          return helper.response(res, 403, error.message)
+        } else {
+          req.decodeToken = result
+          if (req.decodeToken.id_company) {
+            next()
+          } else {
+            return helper.response(res, 403, 'Youre not a worker !')
+          }
+        }
+      })
+    } else {
+      return helper.response(res, 403, 'Please login first !')
+    }
   }
 }
--- src/modules/worker/worker_routes.js
+++ src/modules/worker/worker_routes.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const Route = express.Router()
 const workerController = require('./worker_controller')
-// const authMiddleware = require('../../middleware/auth')
+const authMiddleware = require('../../middleware/auth')
 const uploadFile = require('../../middleware/uploads')
 // const redisMiddleware = require('../../middleware/redis')
 
 Route.get('/', workerController.getWorker)
 Route.get('/:id', workerController.getWorkerById)
-Route.patch('/biodata/:id', uploadFile, workerController.updateWorkerBiodata)
-Route.patch('/skills/:id', workerController.updateWorkerSkills)
-Route.post('/portofolio/:id', uploadFile, workerController.postWorkerPortofolio)
-Route.post('/experience/:id', workerController.postWorkerExperience)
+Route.patch(
+  '/biodata/:id',
+  authMiddleware.isWorker,
+  uploadFile,
+  workerController.updateWorkerBiodata
+)
+Route.patch(
+  '/skills/:id',
+  authMiddleware.isWorker,
+  workerController.updateWorkerSkills
+)
+Route.post(
+  '/portofolio/:id',
+  authMiddleware.isWorker,
+  uploadFile,
+  workerController.postWorkerPortofolio
+)
+Route.post(
+  '/experience/:id',
+  authMiddleware.isWorker,
+  workerController.postWorkerExperience
+)
 
 module.exports = Route
