6054f72732b04e205b10fcf744e7bb0b230341f9
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-05-22 09:49:37 +0700

    some ajustment

--- src/modules/recruiter/recruiter_route.js
+++ src/modules/recruiter/recruiter_route.js
@@ -1,15 +1,7 @@
 const express = require('express')
 const Route = express.Router()
-const uploadFile = require('../../middleware/uploads')
-const redisMiddleware = require('../../middleware/redis')
 const recruiterController = require('./recruiter_controller')
 
-Route.get(
-  '/:id',
-  redisMiddleware.getCompanyRedis,
-  recruiterController.getDataById
-)
-Route.patch('/:id', uploadFile, recruiterController.updateRecruiter)
 Route.post('/hire', recruiterController.hireWorker)
 
 module.exports = Route
